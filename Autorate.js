476a4ba1a4579a123ecd5b04645a334c500be210
emon100 <emon100@qq.com>
2019-05-15 16:42:17 +0800

    Fix.

--- Autorate.js
+++ Autorate.js
@@ -4,16 +4,19 @@
 // @namespace    http://github.com/emon100
 // @version      0.1
 // @description  自动评老师
-// @author       Inspired by DuckSoft & Nekokir & wanghaiwei.Edited by emon100.
+// @author       Inspired by DuckSoft & Nekokir & wanghaiwei. Edited by emon100.
 // @match        *://219.216.96.4/*
 // ==/UserScript==
 
-window.addEventListener('dblclick',function(){
-        console.log("oK!");
+(function() {
+    'use strict';
+    window.addEventListener('dblclick',function(){
         var box = document.getElementById('question-list');
         if (box) {
-            [].forEach.call(document.getElementsByClassName('option-list'), e => {e.children[0].children[0].checked=true});
-            setTimeout("document.getElementById('sub').click()",10);
+                [].forEach.call(document.getElementsByClassName('option-list'), e => {e.children[0].children[0].checked=true});
+                setTimeout("document.getElementById('sub').click()",10);
         }
     });
+})();
+
 
