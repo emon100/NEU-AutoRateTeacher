bc157767047ab7a82a3f927f893a9ed52e92134a
emon100 <emon100@qq.com>
2019-05-15 16:04:25 +0800

    First commit.

---
+++ Autorate.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+// ==UserScript==
+// @name         AutoRateTeacher
+// @namespace    http://github.com/emon100
+// @version      0.1
+// @description  自动评老师
+// @author       Inspired by DuckSoft & Nekokir & wanghaiwei.Edited by emon100.
+// @match        *://219.216.96.4/*
+// ==/UserScript==
+
+window.addEventListener('dblclick',function(){
+        console.log("oK!");
+        var box = document.getElementById('question-list');
+        if (box) {
+                [].forEach.call(document.getElementsByClassName('option-list'), e => {e.children[0].children[0].checked=true});
+        }
+    });
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# NEU-AutoRateTeacher
+
+由[AutoRate4iCourse163](https://github.com/DuckSoft/AutoRate4iCourse163)启发而来
+
+## 使用方法
+
+在油猴里添加此脚本，打开任意一位老师的评价页面在页面空白处双击。
